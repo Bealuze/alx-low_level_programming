2acad222bd2fdfa80c33b9d25b91865f5113ec64
Bealuze <bealuzenebes@gmail.com>
2022-06-03 16:24:30 -0700

    new

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,6 +12,6 @@ int main(void)
 		if (a != 'e' && a != 'q')
 			putchar(a);
 	}
-
+	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- entry
+ * Return: alwaus 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf(a);
+	printf('\n');
+
+	return (0);
+}
