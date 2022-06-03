c7cdf669ea33ce96744d20114916a4a6c9106683
Bealuze <bealuzenebes@gmail.com>
2022-06-03 16:01:59 -0700

    me

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e' && a != 'q')
+			putchar(a);
+	}
+
+	return (0);
+}
