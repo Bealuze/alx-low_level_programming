8479008395d31c28539f06657e27793323d28ecf
Bealuze <bealuzenebes@gmail.com>
2022-06-03 10:09:39 -0700

    nwq

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quoe[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, quote, 59);
+	write(2, quoe, 59);
 	return (1);
 }
