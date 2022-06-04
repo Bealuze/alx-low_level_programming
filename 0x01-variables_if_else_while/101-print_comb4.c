5a35d640faa8ea26a5e9effe52a5ad8103ee81e7
Bealuze <bealuzenebes@gmail.com>
2022-06-04 02:55:21 -0700

    new commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main- entry
+ * Return: alawys 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	int i = 0;
+
+	while (i < 10)
+	{
+		j = 0;
+
+		while (j < 10)
+		{
+			k = 0;
+
+			while (k < 10)
+			{
+				if (i < j && j < k && i != j && j != k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i + j + k != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (0);
+}
