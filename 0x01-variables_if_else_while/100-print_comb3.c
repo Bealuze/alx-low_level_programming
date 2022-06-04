e6cfa77034a06c9c76b7f900d79d9a7fdedbf5b8
Bealuze <bealuzenebes@gmail.com>
2022-06-04 02:30:21 -0700

    nwq

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main- entry
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+			}
+			if (i + j != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
