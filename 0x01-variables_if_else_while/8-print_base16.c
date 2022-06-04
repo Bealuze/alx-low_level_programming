83b1160d7579dd8991bec00babb2f7a973225e30
Bealuze <bealuzenebes@gmail.com>
2022-06-04 01:10:22 -0700

    nwq

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-entry
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+	char a;
+
+	for (x = 0; x <= 9; x++)
+		putchar(x);
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+
+	return (0);
+}
