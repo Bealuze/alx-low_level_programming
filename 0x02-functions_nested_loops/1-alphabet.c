208dcd3261ff7ab325abcb18a26fafafd48ce18b
Bealuze <bealuzenebes@gmail.com>
2022-06-07 13:22:22 -0700

    new

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * main- prints alphabets
+ * return: 0
+ */
+void print_alphabet(void)
+{
+	char lAlphabet;
+
+	for (lAlphabet = 'a'; lAlphabet <= 'z'; lAlphabet++)
+	{
+		_putchar(lAlphabet);
+	}
+	_putcahr('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/*
+ * check the code
+ * */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
