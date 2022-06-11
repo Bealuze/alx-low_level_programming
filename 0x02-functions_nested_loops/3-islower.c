5784eda8a8b3001ab313016434fa6ff457351ace
Bealuze <bealuzenebes@gmail.com>
2022-06-11 07:12:30 -0700

    new

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower- returns 1 for lowercase c.
+ * Return: 1 if c is lowercase or returns 0
+ * @c : is the input
+ */
+int _islower(int c)
+{
+	char c;
+
+	if (c == 'c')
+		return (1);
+	else
+		return (0);
+}
