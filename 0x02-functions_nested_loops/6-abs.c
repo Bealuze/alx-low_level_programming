630c6aa51d479154f7a936724def6f9a6442e058
Bealuze <bealuzenebes@gmail.com>
2022-06-12 02:43:30 -0700

    new

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,12 +5,13 @@
  */
 int _abs(int x)
 {
-	if (x >= 0)
+	if (x > 0)
 		_putchar(x);
+	if (x == 0)
+		 _putchar(x);
 	else if (x < 0)
 	{
-		x = x * (-1);
-		_putchar(x);
+		_putchar(-x);
 	}
 	return (0);
 }
