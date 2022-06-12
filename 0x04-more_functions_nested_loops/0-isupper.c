fcb10d415f9e95c1529ac9aab37430a734654da6
Bealuze <bealuzenebes@gmail.com>
2022-06-12 12:12:57 -0700

    new;

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper- entry point
+ * Return: 0
+ * @c : var
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	return (0);
+}
