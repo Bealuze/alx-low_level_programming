9560b2968fab7c8f6d89025f14f4c2d655e3a81b
Bealuze <bealuzenebes@gmail.com>
2022-06-12 05:31:38 -0700

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,10 +14,27 @@ void times_table(void)
 		while (j <= 9)
 		{
 			x = i * j;
-			_putchar(x);
-			_putchar(',');
+			if (j == 0)
+				_putchar('0' + x);
+			else if (x < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + x);
+			}
+			else 
+			{
+				_putchar('0' + x / 10);
+				_putchar('0' + x % 10);
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			j++;
 		}
+		_putchar('\n');
 		i++;
 	}
 }
