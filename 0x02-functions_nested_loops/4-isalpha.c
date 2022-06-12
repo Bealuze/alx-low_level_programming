5d79abc66a6b6135cc6738be0836da9d6c23f2ac
Bealuze <bealuzenebes@gmail.com>
2022-06-12 02:12:33 -0700

    new

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _islower- returns 1 for lowercase c.
+ * _isalpha- returns 1 for lowercase c.
  * Return: 1 if c is lowercase or returns 0
  * @c : is the input
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
