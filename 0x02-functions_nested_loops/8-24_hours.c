265767cd6385268abd37d9b9f4de45f00e8b0d7a
Bealuze <bealuzenebes@gmail.com>
2022-06-12 03:47:48 -0700

    new

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,18 +6,34 @@
 void jack_bauer(void)
 {
 	int j;
+	int k;
+	int l;
+	int x = 9;
 	int i = 0;
 
-	while (i < 24)
+	while (i < 3)
 	{
+		if (j == 2)
+			x = 3;
 		j = 0;
-
-		while (j < 60)
+		while (j <= x)
 		{
-			_putchar(i);
-			_putchar(':');
-			_putchar(j);
-			_putchar('\n');
+			k = 0;
+			while (k < 5)
+			{
+				l = 0;
+				while (l <= 9)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_putchar('\n');
+					l++;
+				}
+				k++;
+			}
 			j++;
 		}
 		i++;
