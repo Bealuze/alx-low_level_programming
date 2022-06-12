60ae352a52fa2e882cba46495a2d9dc998706c2d
Bealuze <bealuzenebes@gmail.com>
2022-06-12 02:38:44 -0700

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs- absolute value
+ * return: 0
+ */
+int _abs(int)
+{
+	int x;
+	if (x >= 0)
+		_putchar(x);
+	else if (x < 0)
+	{
+		x = x * (-1);
+		_putchar(x);
+	}
+	return (0);
+}
